b5a7f0b2d16acd8804d9658dc4598a9c0fcfc4e3
a1rmanTomer <extramine7@gmail.com>
2024-12-20 04:56:19 +0200

    .

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+//dates and times
 document.addEventListener('DOMContentLoaded', (event) => {
     function updateTime() {
         const now = new Date();
         const hours = String(now.getHours()).padStart(2, '0');
+        const randomHours = Math.floor(Math.random() * 24);
         const minutes = String(now.getMinutes()).padStart(2, '0');
+        const randomMinutes = Math.floor(Math.random() * 60);
         const timeString = `${hours}:${minutes}`;
+        const alarmRanTime = `${randomHours}:${randomMinutes}`;
         const dateString = now.toDateString();
         
         const timeInput = document.getElementById('time');
         const dateInput = document.getElementById('date');
+        const alarmRan = document.getElementById('alarm');
+
+        if (alarmRan) {
+            if (!alarmRan.value) {
+                alarmRan.value = alarmRanTime;
+            }
+        }
+
         if (dateInput) {
             dateInput.value = dateString;
         }
@@ -17,9 +29,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     setInterval(updateTime, 1000);
-    updateTime(); // Initial call to set the time immediately
+    updateTime();
 });
 
+//flashlight
 var flash = false;
 function flash(){
     if (flash = false){
@@ -32,6 +45,7 @@ function flash(){
     }
 }
 
+//camera
 function openCamera(){
     console.log("Opening Camera");
 }
